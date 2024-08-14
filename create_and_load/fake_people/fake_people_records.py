5cd15c611238bf41790d4fd3b4d7463abcfc8b68
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-08-14 16:10:33 +0100

    Add files via upload

---
+++ create_and_load/fake_people/fake_people_records.py
@@ -0,0 +1,139 @@
+"""
+A file that includes functions to help generate records of fake people.
+"""
+
+import csv
+import os
+import sqlite3
+from uuid import uuid4
+
+from faker import Faker
+
+from create_and_load.phone_numbers.phone_number import get_phone_number
+
+CSV_FILE_NAME = "fake_person_data.csv"
+
+
+def get_people(num_of_people_to_generate=10):
+    """
+    Yields a list containing the fake person's fake data as a dictionary
+
+    :param - num_of_people_to_generate (int)
+
+    Returns a generator of people as key, value pairs: \n
+    "id": str, \n
+    "full_name": str, \n
+    "first_name": str, \n
+    "last_name": str, \n
+    "email_address": str, \n
+    "phone_number": str, \n
+    "linkedin_profile": str
+    """
+
+    for i in range(num_of_people_to_generate):
+        full_name = Faker().name()
+
+        if "." in full_name.split(" ")[0]:
+            first_name = full_name.split(" ")[1]
+            last_name = full_name.split(" ")[2]
+        else:
+            first_name = full_name.split(" ")[0]
+            last_name = full_name.split(" ")[1]
+
+        email_address = f"{first_name.lower()}.{last_name.lower()}@example.com"
+        phone_number = get_phone_number()
+        linkedin_profile = f"""
+                wwww.linkedin.com/{first_name.lower()}-{last_name.lower()}
+        """.strip()
+
+        person = {
+            "id": str(uuid4()),
+            "full_name": full_name,
+            "first_name": first_name,
+            "last_name": last_name,
+            "email_address": email_address,
+            "phone_number": phone_number,
+            "linkedin_profile": linkedin_profile,
+        }
+
+        yield person
+
+
+def get_rows_of_people(person_iterator) -> list[list[str]]:
+    """
+    Returns rows of people to add to the database as a list of dictionary values \n
+
+    :param - person_iterator (generator/lazy iterator) a generator storing people to add
+    to the database
+
+    Returns a list of a list of records for each person
+    """
+
+    people = []
+
+    while True:
+        try:
+            people.append(list(next(person_iterator).values()))
+        except StopIteration:
+            return people
+
+
+def write_people_to_csv_file(
+    people: list[list[str]], csv_file_name=CSV_FILE_NAME
+) -> None:
+    """
+    Writes data about a fake person to a csv file (or creates it if it doesn't exist)\n
+
+    :param - people (list of a list of strings) - rows of people to add to the
+    database\n
+    :param - csv_file_name (str) - the name of the csv file you want to create
+    (including the .csv extension)
+    """
+
+    with open(csv_file_name, "a", newline="\n") as file:
+        writer = csv.writer(file, delimiter=",")
+        writer.writerows(people)
+
+
+def load_people_to_db(csv_file_name: str) -> int:
+    """
+    Writes data about a fake people from a csv file to a database \n
+
+    :param - csv_file_name (str) - the csv file to read the rows of people from and
+    write to the database.\n
+
+    returns the number of rows written to the database
+    """
+
+    if "fake_people.db" in os.listdir():
+        os.remove("fake_people.db")
+
+    # Create a database called fake_people
+    con = sqlite3.connect("fake_people.db")
+    cur = con.cursor()
+
+    # Create the table in the fake_people database
+    cur.execute(
+        """
+        CREATE TABLE people(
+            id,
+            full_name,
+            first_name,
+            last_name,
+            email_address,
+            phone_number,
+            linkedin_profile
+        )
+        """
+    )
+
+    # Insert the data about the fake people from the csv into the table
+    with open(csv_file_name, "r", newline="\n") as file:
+        reader = csv.reader(file, delimiter=",")
+        data = [(row) for row in reader]
+
+    cur.executemany("INSERT INTO people VALUES(?, ?, ?, ?, ?, ?, ?)", data)
+    con.commit()
+    con.close()
+
+    return len(data)
--- create_and_load_data.py
+++ create_and_load_data.py
@@ -3,16 +3,19 @@ The main .py file to run which creates records of fake people, writes them to a 
 file and then writes these records of fake people from the .csv file to a database.
 """
 
-import csv
 import logging
 import os
-import sqlite3
 
-from fake_people.fake_people_records import get_people, get_rows_of_people
+from create_and_load.fake_people.fake_people_records import (
+    CSV_FILE_NAME,
+    get_people,
+    get_rows_of_people,
+    load_people_to_db,
+    write_people_to_csv_file,
+)
 
 # Python Basics: Python scope and LEGB rule
 # Global variables
-CSV_FILE_NAME = "fake_person_data.csv"
 NUM_OF_PEOPLE_TO_GENERATE = 1000
 LOGGER = logging.getLogger(__name__)
 
@@ -26,67 +29,6 @@ logging.basicConfig(
     encoding="utf-8",
     level=logging.INFO,
 )
-
-
-def write_people_to_csv_file(
-    people: list[list[str]], csv_file_name=CSV_FILE_NAME
-) -> None:
-    """
-    Writes data about a fake person to a csv file (or creates it if it doesn't exist)\n
-
-    :param - people (list of a list of strings) - rows of people to add to the
-    database\n
-    :param - csv_file_name (str) - the name of the csv file you want to create
-    (including the .csv extension)
-    """
-
-    with open(csv_file_name, "a", newline="\n") as file:
-        writer = csv.writer(file, delimiter=",")
-        writer.writerows(people)
-
-
-def load_people_to_db(csv_file_name: str) -> int:
-    """
-    Writes data about a fake people from a csv file to a database \n
-
-    :param - csv_file_name (str) - the csv file to read the rows of people from and
-    write to the database.\n
-
-    returns the number of rows written to the database
-    """
-
-    if "fake_people.db" in os.listdir():
-        os.remove("fake_people.db")
-
-    # Create a database called fake_people
-    con = sqlite3.connect("fake_people.db")
-    cur = con.cursor()
-
-    # Create the table in the fake_people database
-    cur.execute(
-        """
-        CREATE TABLE people(
-            id,
-            full_name,
-            first_name,
-            last_name,
-            email_address,
-            phone_number,
-            linkedin_profile
-        )
-        """
-    )
-
-    # Insert the data about the fake people from the csv into the table
-    with open(csv_file_name, "r", newline="\n") as file:
-        reader = csv.reader(file, delimiter=",")
-        data = [(row) for row in reader]
-
-    cur.executemany("INSERT INTO people VALUES(?, ?, ?, ?, ?, ?, ?)", data)
-    con.commit()
-    con.close()
-
-    return len(data)
 
 
 if __name__ == "__main__":
